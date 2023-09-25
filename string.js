d49de51b8acf13ae587450aa880a40a345179082
Ravi Kumar <78343870+Ravikumar-4142@users.noreply.github.com>
2023-09-25 04:51:09 +0000

    string split methode

--- string.js
+++ string.js
@@ -1,10 +1,10 @@
-const name = "         Ravi          "
-const repoCount = 50
+// const name = "         Ravi          "
+// const repoCount = 50
 
 // String concatenation
 // console.log(`My Name is ${name} and my repo are ${repoCount}`)
 
-const gameName  = new String("Forza-      Horizon")
+// const gameName  = new String("Forza-      Horizon")
 // console.log(gameName[1])
 // console.log(gameName.__proto__)
 // console.log(gameName.length)
@@ -14,14 +14,20 @@ const gameName  = new String("Forza-      Horizon")
 // const newString = gameName.substring(0,4)
 // const newString = gameName.slice(-4, 4)
 
-const newString =  name.trim()
-setTimeout(function(){
-    console.log(name)    
-},
-5000);
-console.log(newString)
+// const newString =  name.trim()
+// setTimeout(function(){
+//     console.log(name)    
+// },
+// 5000);
+// console.log(newString)
 /// settimeout function clear
 
-const url = "https:://ravi.com/ravi%20kumar"
-console.log(url.replace('%20','-'))
+// const url = "https:://ravi.com/ravi%20kumar"
+// console.log(url.replace('%20','-'))
+
+const name  = new String("Hello-Ravi-Kumar")
+const name1  = "Hello-Ravi-Kumar"
+console.log(name.split('-'))
+
+
 
