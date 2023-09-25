69b4e8ee4bb23c16987d522d5b4c866f134c507a
Ravi Kumar <78343870+Ravikumar-4142@users.noreply.github.com>
2023-09-25 03:50:00 +0000

    string study done

---
+++ string.js
@@ -0,0 +1,20 @@
+const name = "         Ravi          "
+const repoCount = 50
+// console.log(`My Name is ${name} and my repo are ${repoCount}`)
+
+const gameName  = new String("Forza-      Horizon")
+// console.log(gameName[1])
+// console.log(gameName.__proto__)
+// console.log(gameName.length)
+// console.log(gameName.toLowerCase())
+// console.log(gameName.charAt(0))
+// console.log(gameName.indexOf('H'))
+// const newString = gameName.substring(0,4)
+// const newString = gameName.slice(-4, 4)
+
+// const newString =  name.trim()
+// console.log(name)
+// console.log(newString)
+
+const url = "https:://ravi.com/ravi%20kumar"
+console.log(url.replace('%20','-'))
