3143e31c4a910e0d86a11d15cea9cd7325e77150
Ravi Kumar <kumarsravi397@gmail.com>
2023-09-26 09:36:25 +0530

    scpe variable

---
+++ scope.js
@@ -0,0 +1,16 @@
+ var c = 30
+let a = 100
+if (true) {
+    let a = 10
+    const b = 20
+   console.log('INNER ', a)
+}
+
+console.log(a)
+// console.log(b)
+// console.log(c)
+
+for (let i = 0; i < Array.length; i++) {
+    const element = array[i];
+}
+
