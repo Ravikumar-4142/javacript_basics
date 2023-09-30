4853999223877f81c08fc4478db2a2b848029a13
Ravi Kumar <kumarsravi397@gmail.com>
2023-09-30 12:04:40 +0530

    while loop

--- loops.js
+++ loops.js
@@ -20,7 +20,7 @@
 
 
 /// Arrays Loops
-// let myArray = ['IronMan', 'Thor', 'Captain America', 'Black Widow', 'Howkey']
+let myArray = ['IronMan', 'Thor', 'Captain America', 'Black Widow', 'Howkey']
 // // print all values
 // for (let index = 0; index < myArray.length; index++) {
 //     const element = myArray[index];
@@ -45,11 +45,36 @@
 //     console.log(element);
 // }
 
-for (let i = 0; i <= 10; i++) {
-    const element = i;
-    if (element == 5) {
-        console.log(`Number ${element} is best number`);
-        continue
-    }
-    console.log(element);
+// for (let i = 0; i <= 10; i++) {
+//     const element = i;
+//     if (element == 5) {
+//         console.log(`Number ${element} is best number`);
+//         continue
+//     }
+//     console.log(element);
+// }
+
+
+
+// ============ While Loop ============
+
+let index = 0
+// while (index <= 10) {
+//     console.log(`Value of index: ${index} `);
+//     index = index + 2
+// }
+let arr = 0
+while (arr < myArray.length) {
+    console.log(`Value is: ${myArray[arr]} `);
+    arr = arr + 1;
 }
+
+
+// Do While Loop
+let score = 1
+do {
+    console.log(`Value is: ${score} `);
+    score++
+} while (score <= 10);
+
+// ForEach loop
