f6ba35377b20350fbe02296af312f9a22d87f9d2
Ravi Kumar <kumarsravi397@gmail.com>
2023-09-30 13:30:30 +0530

    basic done

--- loops.js
+++ loops.js
@@ -145,21 +145,118 @@ const programming = ['js', 'php', 'py', 'java', 'c', 'cpp']
 //     console.log(item, index, arr);
 // });
 
-const coding = [
+// const coding = [
+//     {
+//         languagename: "PHP",
+//         languagefilename: ".php"
+//     },
+//     {
+//         languagename: "JavaScript",
+//         languagefilename: ".js"
+//     },
+//     {
+//         languagename: "Python",
+//         languagefilename: ".py"
+//     }
+// ]
+
+// coding.forEach((item) => {
+//     console.log(item.languagename);
+// })
+
+
+
+
+// other coding
+
+// const values = programming.forEach((item) => {
+//     // console.log(item);
+//     return item
+// })
+
+// console.log(values);  // return undefined
+/// ====== Filter =================== return with conditions
+const myNumbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+// const newNums = myNumbers.filter((num) => num > 4)
+// console.log(newNums);
+
+
+// const newNums = myNumbers.filter((num) => {
+//     return num > 4
+// })
+// console.log(newNums);
+
+// const myNewNums = []
+// myNumbers.forEach((num) => {
+//     if (num > 4) {
+//         myNewNums.push(num)
+//     }
+    
+// })
+// console.log(myNewNums)
+
+
+// const books = [
+//     { title: 'Book One', genre: 'Fiction', publish: 1981, edition: 2004 },
+//     { title: 'Book Two', genre: 'Non-Fiction', publish: 1992, edition: 2008 },
+//     { title: 'Book Three', genre: 'History', publish: 1999, edition: 2007 },
+//     { title: 'Book Four', genre: 'Non-Fiction', publish: 1989, edition: 2010 },
+//     { title: 'Book Five', genre: 'Science', publish: 2009, edition: 2014 },
+//     { title: 'Book Six', genre: 'Fiction', publish: 1987, edition: 2010 },
+//     { title: 'Book Seven', genre: 'History', publish: 1986, edition: 1996 },
+//     { title: 'Book Eight', genre: 'Science', publish: 2011, edition: 2016 },
+//     { title: 'Book Nine', genre: 'Non-Fiction', publish: 1981, edition: 1989 },
+//   ];
+
+// let userBooks = books.filter( (bk) => bk.genre === 'History')
+
+// userBooks = books.filter( (bk) => {
+//     return bk.publish >= 1995 && bk.genre === "History"
+// })
+//   console.log(userBooks);
+
+
+// ======== Map ===============
+
+// const mynewmyNumbers = myNumbers.map( (num) => {return num +  10})
+// console.log(mynewmyNumbers);
+// ==================== Chaining methods ====================
+// const mynewmyNumbers = myNumbers.map((num) => num * 10)
+//     .map((num) => num + 1)
+//     .filter( (num) => num >= 10)
+// console.log(mynewmyNumbers);
+
+/// ============ Reduce() ===================
+
+const arrayNum = [1, 2, 3]
+// const myTotal = arrayNum.reduce(function (acc, cval) {
+//     console.log(`acc : ${acc} cval: ${cval}`)
+//     return acc + cval
+// }, 0)
+
+// const myTotal = arrayNum.reduce( (acc, cval) => acc + cval, 0)
+// console.log(myTotal)
+
+const shoppingCart = [
     {
-        languagename: "PHP",
-        languagefilename: ".php"
+        itemname: "JS Course",
+        price: 599,
     },
     {
-        languagename: "JavaScript",
-        languagefilename: ".js"
+        itemname: "Python Course",
+        price: 599,
     },
     {
-        languagename: "Python",
-        languagefilename: ".py"
-    }
+        itemname: "JAVA Course",
+        price: 1499,
+    },
+    {
+        itemname: "Data Scientist",
+        price: 12999,
+    },
 ]
 
-coding.forEach((item) => { 
-    console.log(item.languagename);
-})
+const shopingPay = shoppingCart.reduce((acc, item) => acc + item.price, 0)
+
+console.log(shopingPay);
