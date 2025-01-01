d566471e37d4a1ce822712e7204ba4d6cb227e0b
Ravi Kumar <ravi.kumar1@lyxelandflamingo.com>
2025-01-01 23:33:51 +0530

    convesrion of datatypes

---
+++ 01_datatype_conversion.js
@@ -0,0 +1,41 @@
+// data type conversion
+// 1. String
+let value = "33"; // we can check by changing value to any other data type
+console.log(typeof value); // string
+console.log(typeof(value));
+
+// convert string to number
+let valueInNumber = Number(value);
+console.info(valueInNumber); // 0
+console.log(typeof valueInNumber); // number
+
+// convert string to boolean
+let valueInBoolean = Boolean(value);
+
+
+
+// from Hitesh Sir
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// === 
+
+console.log("2" === 2);
+
+
+
